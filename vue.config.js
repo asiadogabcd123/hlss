f571809a7dadbe23c7149d9cf472b0011055ab27
asiadogabcd123 <43464597+asiadogabcd123@users.noreply.github.com>
2025-07-05 10:46:34 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    host: '0.0.0.0', // 允许局域网访问
+    port: 8081,      // 固定端口
+    hot: true,
+    allowedHosts: 'all' // 允许所有主机访问
+  }
   
 })
